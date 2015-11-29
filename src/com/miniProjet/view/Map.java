96f91e00304ce4e348991f1bcd25e8003a00b01e
PetitBai <petitbai@163.com>
2015-11-29 21:19:52 +0100

    find the node

--- src/com/miniProjet/view/Map.java
+++ src/com/miniProjet/view/Map.java
@@ -3,9 +3,13 @@ package com.miniProjet.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -39,20 +43,39 @@ public class Map extends JFrame{
 		jToolBar.add(zoomButton2);
 		jToolBar.add(selectArc);
 		jToolBar.add(selectNode);
-		selectNode.addActionListener(new ActionListener() {		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-		        // s save the number inputed by user
-		        String s=JOptionPane.showInputDialog("Please input the id of node:");
-			}
-		});
+
 		/****************************************************************/
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 		frame.add(jToolBar, BorderLayout.PAGE_START);
-		frame.add(new graphics());
+		
+		
+		graphics mapGraphics = new graphics();		
+		frame.add(mapGraphics);
+				
+		
+	
 		//frame.setContentPane(new graphics());//add map to framework
+
+		/****************************************************************/
+		selectNode.addActionListener(new ActionListener() {		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+		        // s save the number inputed by user
+		        String s=JOptionPane.showInputDialog("Please input the id of node:");
+		        int pointId = Integer.parseInt(s);	
+		        //make a new JPanel with the node found
+		        pointFound hello = new pointFound(pointId);		       
+		        frame.add(hello);
+		        hello.updateUI();
+		   	        
+		        
+			}
+		});
+		
+		
+		
 	}
 }
 
@@ -64,13 +87,69 @@ class graphics extends JPanel {
 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
 		ReadData.readNode(ReadData.NODE_PATH);//read data
 		MakePoint.makeScreenPoint();//get points
 	
 		for(int i = 0;i<MakePoint.point.size();i++){
 			g.drawString(".",MakePoint.point.get(i).x,MakePoint.point.get(i).y);//draw map
+
 			g.setColor(Color.RED);//set brush's color
 		}
+		
+
+	}
+}
+
+class pointFound extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int pointID;
+	
+	public pointFound(int i) {
+		pointID = i;
+	}
+
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		ReadData.readNode(ReadData.NODE_PATH);//read data
+		MakePoint.makeScreenPoint();//get points
+		
+		for(int i = 0;i<MakePoint.point.size();i++){
+			g.drawString(".",MakePoint.point.get(i).x,MakePoint.point.get(i).y);//draw map
+
+			g.setColor(Color.RED);//set brush's color
+		}
+		
+		
+//		/**
+//		 * Use ImageIcon to draw the image PointFound
+//		 */
+//		ImageIcon icon = new ImageIcon("Images/dot.jpeg");	
+//		Image imgDot = icon.getImage();
+//		g.drawImage(imgDot, 500, 500, null);
+		
+		/**
+		 * Use ImageIO to get the image PointFound
+		 */
+		if (pointID >= 0 && pointID < MakePoint.point.size()) {
+			try {
+				//draw the picture
+				Image imgDot2 = ImageIO.read(new File("Images/dot.jpeg"));
+				g.drawImage(imgDot2, MakePoint.point.get(pointID).x, MakePoint.point.get(pointID).y,20,30, null);
+				g.setColor(Color.BLACK);
+				//draw the latitude
+				g.drawString(Integer.toString(MakePoint.point.get(pointID).x), MakePoint.point.get(pointID).x+30, MakePoint.point.get(pointID).y + 50);
+				//draw the longtitude
+				g.drawString(Integer.toString(MakePoint.point.get(pointID).y), MakePoint.point.get(pointID).x+30, MakePoint.point.get(pointID).y + 70);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
 
